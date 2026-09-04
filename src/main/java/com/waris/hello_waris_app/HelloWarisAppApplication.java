8992f45d4c9da3dd7ad129c532d794087ea58cb5
Waris Ali <warissid41@gmail.com>
2026-09-04 18:52:05 +0530

    Initial commit

---
+++ src/main/java/com/waris/hello_waris_app/HelloWarisAppApplication.java
@@ -0,0 +1,13 @@
+package com.waris.hello_waris_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWarisAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWarisAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/waris/hello_waris_app/HelloWarisAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.waris.hello_waris_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWarisAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
